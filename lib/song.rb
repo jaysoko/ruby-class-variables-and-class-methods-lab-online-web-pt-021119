16341fef757df51f918c4ff2b5088bb799fd5be2
Jay Sokolowski <jaysoko@gmail.com>
2019-03-15 00:21:13 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,47 @@
+class Song
+	@@count = 0
+	@@genres = []
+        @@artists = []
+attr_accessor :name,:artist,:genre
+
+def initialize(name,artist,genre)
+   @name = name
+   @artist = artist
+   @genre = genre
+   @@count += 1
+##   @@genres.push(genre) unless @@genres.include?(genre)
+##   @@artists.push(artist)unless @@artists.include?(artist) 
+   @@genres.push(genre)
+   @@artists.push(artist)
+end
+
+def self.count
+  @@count
+end
+
+def self.artists
+   @@artists.uniq
+end
+
+def self.genres
+  @@genres.uniq
+end
+
+def self.artist_count
+count = 0
+@@artist_count = {}
+@@artists.each {|x| @@artist_count[:x] }
+
+@@artists.to_h 
+
+
+end 
+
+def self.genre_count
+@@genre_count = {}
+@@genres.map {|x| @@genre_count[:x] }
+
+
+end
+
+end
